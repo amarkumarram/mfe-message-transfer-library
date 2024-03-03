66e7a4bef95f9d85b853f01364be0d1ff4701e89
Amar Kumar Ram <ram.amarkumar@gmail.com>
2024-03-03 10:02:03 +0530

    method changes related to microfrontend communication

--- projects/data-transfer/src/lib/data-transfer.service.ts
+++ projects/data-transfer/src/lib/data-transfer.service.ts
@@ -1,13 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTransferService {
+  
+  private hostData = new BehaviorSubject({});
+  private mfeData = new BehaviorSubject({});
 
   constructor() { }
 
-  logMessage(message: any) {
-    console.log("This is from librar: ", message);
+  // This is to set data at HOST side
+  setHostToMfeData(data: any) {
+    this.hostData.next({
+      value: data
+    })
+  }
+
+  // This is to get data at MFE side that is set at HOST side
+  getHostToMfeData() {
+    return this.hostData;
+  }
+
+  // This is to set data at MFE side
+  setMfeToHostData(data: any) {
+    this.mfeData.next({
+      value: data
+    })
+  }
+
+  // This is to get data at HOST side that is set at MFE side
+  getMfeToHostData() {
+    return this.mfeData;
   }
 }
